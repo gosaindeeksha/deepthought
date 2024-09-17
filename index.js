7b41144b706d9452283646372f4de9eb88efe08f
gosaindeeksha <148262951+gosaindeeksha@users.noreply.github.com>
2024-09-18 00:21:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,166 @@
+var data = {
+    "_id": {
+    "$oid": "63b64dc9e3b230ebb60a495d"
+    },
+    "_key": "topic:2085",
+    "category": "Course",
+    "cid": {
+    "$numberDouble": "NaN"
+    },
+    "commitment": "4 hours",
+    "commitment_type": "custom",
+    "deadline": "",
+    "description": "<p>Have you ever thought, Pareto's Law can be applied to cooking? Your thinking starts when you start thinking beyond your thinking.</p>\n<p>Let's prepare Sandwiches, we will use Pareto&rsquo;s Law. That&rsquo;s an 80-20 approach.<br>80% of the time in researching, and planning and 20% of the time in implementation.</p>\n<p>So for preparing sandwiches, we need vegetables, bread, cheese, butter and much more. So initially we collect all the stuff and then chop the vegetables, grate the cheese, and make a mash of potato. So this all comes in 80% and then comes 20% where we will roast the bread, spread the sauce, place the mash put some cheese, and heat it for a while and our sandwich is ready.</p>\n<p>Similarly while creating any functionality 80% of the time is spent on consequences that might appear, some parameters we have to take care of, the scope of the variable, and some dependent functions, and then 20% comes from implementation.</p>\n<p>Let's start thinking together, and search for how you can get the essence of project management.</p>",
+    "faqs": [],
+    "globalTags": [],
+    "isActive": true,
+    "lastposttime": 0,
+    "learning_outcomes": [
+    "Bare minimum knowledge of project management",
+    "4SA Concept",
+    "Would be able to handle any project efficiently"
+    ],
+    "mainPid": 0,
+    "postcount": 0,
+    "pre_requisites": [
+    "An open mind to learn any concept",
+    "Thought of Unlearning and Relearning "
+    ],
+    "project_image": "https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/1114276/0413_What_is_a_Technical_Project_Manager_Luke_Social-21e35c2d76465934c0f844c396db762a.png",
+    "short_description": "You can learn project management by applying the simple methods of project management. How you can apply project management in each and every step of your deliverables? Let's figure it out together",
+    "slug": "2085/technical-project-management",
+    "status": "published",
+    "tasks": [
+    {
+    "task_id": 18882,
+    "task_title": "Explore the world of management",
+    "task_description": "As a project manager, you play an important role in leading a project through initiation, planning, execution, monitoring, controlling and completion. How? Do you want to manage each and every step of your life?",
+    "status": "notworkyet",
+    "assets": [
+    {
+    "asset_id": 18883,
+    "asset_title": "Technical Project Management",
+    "asset_description": "Story of Alignment\r\nScope of Agility\r\nSpecific Accountable \r\nStaggering Approach\r\n\r\n",
+    "asset_content": "  <iframe width='480' height='315' src='https://www.youtube.com/embed/TiMRwri1xJ8?si=t0bUZqdtLBFvf9H2' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>",
+    "asset_type": "display_asset",
+    "asset_content_type": "video"
+    },
+    {
+    "asset_id": 18884,
+    "asset_title": "Threadbuild",
+    "asset_description": "Watch the video and thread build, and jot out key threads while watching that video.",
+    "asset_content": " <div class='thread-container'><div class='thread-a'><i class='fas fa-chevron-up'></i> <h2>Thread A</h2></div><div class='sub'><div class='sub-thread'> <label for='sub-thread-1'>Sub thread 1</label> <textarea id='sub-thread-1' placeholder='Enter Text here'></textarea>  </div><div class='sub-interpretation'> <label for='sub-interpretation-1'>Sub Interpretation 1</label> <textarea id='sub-interpretation-1' placeholder='Enter Text here'></textarea>  </div> </div><div class='toggle-container'> <div class='thread-icons'>  <img src='assets/bulb.svg' alt='light bulb'> <img src='assets/message.svg' alt='comment'> <img src='assets/discussion.svg' alt='discussion'> <img src='assets/flower.svg' alt='flower'></div> <select class='dropdown'> <option>Select Category</option> </select> <select class='dropdown'><option>Select Process</option> </select> </div> <button class='sub-thread-btn submit'>+ Sub-thread</button> <div class='sum-thread'> <label for='sum-thread1'>Summary of Thread A</label> <textarea id='sum-thread1' placeholder='Enter Text here'></textarea></div></div>",
+    "asset_type": "input_asset",
+    "asset_content_type": "threadbuilder"
+    },
+    {
+    "asset_id": 18885,
+    "asset_title": "Structure you pointers ",
+    "asset_description": "Write a 400-500 word article, from your thread. Publish your understanding, and showcase your learning to the entire world.",
+    "asset_content": "<div class='editor-container'><label for='title' class='input-label'>Title</label><input type='text' id='title' class='input-field' placeholder='Enter the title'><label for='content' class='input-label'>Content</label><!-- Custom top menu bar --><div class='menu-bar'><div><button>File</button><button>Edit</button><button>View</button><button>Insert</button><button>Format</button><button>Tools</button><button>Table</button><button>Help</button></div></div><!-- Quill custom toolbar (bottom row) --><div id='toolbar' class='custom-toolbar'><span class='ql-formats'><button class='ql-undo'><img src='assets/arrow-left.svg' alt='undo'></button><button class='ql-redo'><img src='assets/arrow-right.svg' alt='redo'></button><button class='ql-fullscreen'><img src='assets/expand.svg' alt='fullscreen'></button></span><select class='ql-header'><option value='1'>Heading 1</option><option value='2'>Heading 2</option><option value='' selected>Paragraph</option></select><button><i class='fa-solid fa-ellipsis' style='color: #616161;'></i></button></div><!-- Quill editor container --><div id='editor' class='quill-editor'></div></div><script src='https://cdn.quilljs.com/1.3.6/quill.js'></script> ",
+    "asset_type": "input_asset",
+    "asset_content_type": "article"
+    },
+    {
+    "asset_id": 18886,
+    "asset_title": "4SA Method",
+    "asset_description": "To explore more read more",
+    "asset_content": " <div class='container'><!-- Introduction Section --><div class='section'><button class='collapsible'><span>Introduction</span><i class='fas fa-angle-up'></i></button><div class='sa-content'><p>The 4SA Method, How to bring an idea into progress?</p><a href='#'>See More</a></div></div><!-- Thread A Section --><div class='section'><button class='collapsible'><span>Thread A</span><i class='fas fa-angle-up'></i></button><div class='sa-content'><p>How are you going to develop your strategy? Which method are you going to use to develop a strategy? What if the project is lengthy?</p><a href='#'>See More</a></div></div><!-- Example 1 Section --><div class='section'><button class='collapsible'><span>Example 1</span><i class='fas fa-angle-up'></i></button><div class='sa-content'><p>You have a concept, how will you put it into progress?</p><a href='#'>See More</a></div></div></div>",
+    "asset_type": "display_asset",
+    "asset_content_type": "article"
+    }
+    ]
+    }
+    ],
+    "tid": 2085,
+    "timestamp": 1672891849700,
+    "title": "Technical Project Management",
+    "type": "project",
+    "uid": 100,
+    "viewcount": 0,
+    "publishedAt": 1672893847792
+    }
+
+ document.getElementById("task_name").innerHTML = data.title;
+ document.querySelector(".grey .task_title").innerHTML = data.tasks[0].task_title;
+ document.querySelector(".grey .task_description").innerHTML = data.tasks[0].task_description;
+
+// JS for adding Journey Board content
+var Journey_content = document.querySelector('.journey-board-content');
+Journey_content.innerHTML = `<li><b>${data.tasks[0].task_title}</b></li>`;
+data.tasks[0].assets.forEach((asset)=>{
+    Journey_content.innerHTML+=`<li>${asset.asset_title}</li>
+    `;
+})
+
+// JS for adding card content
+
+var card_container = document.querySelector('.card-container');
+data.tasks[0].assets.forEach((asset)=>{
+    card_container.innerHTML+=` <div class="card">
+            <div class="black-heading">
+                <h3>${asset.asset_title}</h3>
+                <i class="fa-solid fa-circle-info fa-lg" style="color: #ffffff;"></i>
+            </div>
+            <div class="description">
+                <p><b>Description: </b>${asset.asset_description}</p>
+
+            </div>
+            <div class="content">
+               ${asset.asset_content}
+            </div>
+        </div>
+    `;
+})
+
+// JS for JOurney Board
+document.querySelector(".journey-board-btn").addEventListener("click",()=>{
+    document.querySelector(".journey-board-btn").classList.toggle('rotate');
+    document.querySelector(".journey-board").classList.toggle('translat');
+})
+
+//  QUILL JS
+
+var quill = new Quill('#editor', {
+    theme: 'snow',
+    modules: {
+      toolbar: {
+        container: '#toolbar',
+        handlers: {
+          undo: function() {
+            this.quill.history.undo();
+          },
+          redo: function() {
+            this.quill.history.redo();
+          },
+          fullscreen: function() {
+            document.querySelector('.editor-container').requestFullscreen();
+          }
+        }
+      }
+    }
+  });
+
+  // Add custom handlers for undo, redo, and fullscreen
+  document.querySelector('.ql-undo').addEventListener('click', function() {
+    quill.history.undo();
+  });
+
+  document.querySelector('.ql-redo').addEventListener('click', function() {
+    quill.history.redo();
+  });
+
+// Collapsible js
+const collapsibles = document.querySelectorAll(".collapsible");
+
+    collapsibles.forEach(button => {
+        button.addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    });
